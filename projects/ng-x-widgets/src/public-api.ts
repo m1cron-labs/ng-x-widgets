043d0bf2d087ab8186c04a3f598f5398916c0972
Nikes <balashovalex858@gmail.com>
2025-01-18 15:41:39 +0200

    fix: public-api

--- projects/ng-x-widgets/src/public-api.ts
+++ projects/ng-x-widgets/src/public-api.ts
@@ -2,6 +2,9 @@
  * Public API Surface of ng-x-widgets
  */
 
+export * from './lib/models/button-params';
+export * from './lib/models/widgets-version';
+export * from './lib/models/widgets';
 export * from './lib/ng-x-widgets.service';
 
 export * from './widgets';
